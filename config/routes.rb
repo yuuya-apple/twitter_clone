f29ed947f1ff6210700d4e09fa1f5fbcc3b3fad7
kawa <yuuya.apple@me.com>
2024-06-15 08:31:34 +0900

    user_view_controllerテンプレート

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ db/migrate/20240614230107_add_column_user_tell_num_bod.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddColumnUserTellNumBod < ActiveRecord::Migration[7.0]
+  def change
+    change_table :users do |t|
+      t.integer :tell_number, null: false
+      t.date :date_of_birth, null: false
+    end
+  end
+end
