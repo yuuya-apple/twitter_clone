e7f5aa5d57c755f7d0105f3f0f9c9cde5731a4cf
kawa <yuuya.apple@me.com>
2024-06-15 09:27:07 +0900

    必須条件：電話番号/生年月日

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :tell_number, presence: true
+  validates :date_of_birth, presence: true
 end
