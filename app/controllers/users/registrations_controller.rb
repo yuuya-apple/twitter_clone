353e60496658023f31858d5a35fcc042c7258a46
kawa <yuuya.apple@me.com>
2024-06-18 16:56:13 +0900

    rubocop

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    # before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_sign_up_params, only: [:create] # rubocop:disable Rails/LexicallyScopedActionFilter
     skip_before_action :require_login
 
     # before_action :configure_account_update_params, only: [:update]
