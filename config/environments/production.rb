5da99fb23f80a5138b1eb0b34fb08c11d021e5ff
kawa <yuuya.apple@me.com>
2024-06-20 17:39:36 +0900

    heroku mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,17 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'gentle-sands-35485.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener_web
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
