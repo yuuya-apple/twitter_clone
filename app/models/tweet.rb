7ddfb002f014444a17a38136ecb4db5836178cb3
kawa <yuuya.apple@me.com>
2024-06-25 04:20:19 +0900

    model ファボリツ

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,7 @@
 
 class Tweet < ApplicationRecord
   belongs_to :user
+  has_many :children, class_name: 'Tweet',
+                      foreign_key: 'reply_to_id', dependent: :nullify
+  belongs_to :parent, class_name: 'Tweet', optional: true
 end
