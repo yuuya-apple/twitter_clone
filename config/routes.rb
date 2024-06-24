0788f8f81062beceb41b9d0e413f224adab23230
kawa <yuuya.apple@me.com>
2024-06-25 04:13:54 +0900

    プロフィール実装

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     confirmations: 'users/confirmations'
   }
 
+  resources :users, only: [:show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
