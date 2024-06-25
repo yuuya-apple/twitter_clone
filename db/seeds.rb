a01b43d64b670d4d293ba7480325467135f42ded
kawa <yuuya.apple@me.com>
2024-06-26 00:23:02 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,93 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.create!(
+def create_tweet(user, num)
+  Tweet.create!(
+    user_id: user.id,
+    content: "#{num}回目これは本文になります。テスト用の本文です。意味のない本文です。文字数稼ぎの本文です。なかなか文字数稼げてない本文です。これは本文になります。テスト用の本文です。意味のない本文です"
+  )
+end
+
+user1 = User.find_by(email: 'hoge@hoge.com')
+user1.destroy if user1.present?
+user2 = User.find_by(email: 'hogehoge@hoge.com')
+user2.destroy if user2.present?
+user3 = User.find_by(email: 'hogehogehoge@hoge.com')
+user3.destroy if user3.present?
+
+user1 = User.create!(
   email: 'hoge@hoge.com',
   tell_number: 11_111,
   date_of_birth: '2024-06-23',
   display_name: 'ほげ太郎',
-  password: 'password'
+  password: 'password',
+  introduction: '自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。',
+  location: '東京都葛飾区亀有',
+  own_site: 'https://qiita.com/yuuya-apple'
 )
+user1.icon_image.attach(io: File.open(Rails.root.join('app/assets/images/icon_business_man02.png')),
+                        filename: 'icon_business_man02.png')
+user1.header_image.attach(io: File.open(Rails.root.join('app/assets/images/bg_himawari_hatake.jpg')),
+                          filename: 'bg_himawari_hatake.jpg')
+user1.save
+
+user2 = User.create!(
+  email: 'hogehoge@hoge.com',
+  tell_number: 11_111,
+  date_of_birth: '2024-06-23',
+  display_name: 'ほげ次郎',
+  password: 'password',
+  introduction: '自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。',
+  own_site: 'https://qiita.com/yuuya-apple'
+)
+user2.icon_image.attach(io: File.open(Rails.root.join('app/assets/images/icon_business_man06.png')),
+                        filename: 'icon_business_man06.png')
+user2.save
+
+user3 = User.create!(
+  email: 'hogehogehoge@hoge.com',
+  tell_number: 11_111,
+  date_of_birth: '2024-06-23',
+  display_name: 'ほげ三郎',
+  password: 'password',
+  introduction: '自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。自己紹介です。'
+)
+user3.icon_image.attach(io: File.open(Rails.root.join('app/assets/images/icon_business_man07.png')),
+                        filename: 'icon_business_man07.png')
+user3.save
 
 30.times do |n|
-  Tweet.create!(
-    user_id: user.id,
-    content: "#{n}回目。これは本文になります。テスト用の本文です。意味のない本文です。文字数稼ぎの本文です。なかなか文字数稼げてない本文です。これは本文になります。テスト用の本文です。意味のない本文です"
+  create_tweet(user1, n)
+  create_tweet(user2, n)
+  create_tweet(user3, n)
+end
+
+user2.tweets.limit(15).each do |t|
+  Favorite.create!(
+    user_id: user1.id,
+    tweet_id: t.id
+  )
+  Reply.create!(
+    user_id: user1.id,
+    tweet_id: t.id
+  )
+  Retweet.create!(
+    user_id: user1.id,
+    tweet_id: t.id
+  )
+end
+
+user3.tweets.limit(15).each do |t|
+  Favorite.create!(
+    user_id: user1.id,
+    tweet_id: t.id
+  )
+  Reply.create!(
+    user_id: user1.id,
+    tweet_id: t.id
+  )
+  Retweet.create!(
+    user_id: user1.id,
+    tweet_id: t.id
   )
 end
